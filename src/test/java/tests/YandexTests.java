0554d17c387ded202cfe2bb611496bb123c178b2
Andrey Kulikov <9230697@gmail.com>
2020-09-04 15:36:42 +0300

    1

--- src/test/java/tests/YandexTests.java
+++ src/test/java/tests/YandexTests.java
@@ -15,7 +15,7 @@ public class YandexTests {
         $("#text").val("Selenide").pressEnter();
 
         // Проверить, что Selenide появился в результатах поиска
-        $("html").shouldHave(text("ru.selenide.org"));
+        $("html").shouldHave(text("selenide.org"));
     }
 
 }
