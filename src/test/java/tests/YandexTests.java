a1509f7dd75daa62e5a16dca3bcf8af1d97214c9
Andrey Kulikov <9230697@gmail.com>
2020-09-04 15:29:14 +0300

    1

--- src/test/java/tests/YandexTests.java
+++ src/test/java/tests/YandexTests.java
@@ -3,7 +3,6 @@ package tests;
 import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.Condition.text;
-import static com.codeborne.selenide.Selectors.byName;
 import static com.codeborne.selenide.Selenide.*;
 
 public class YandexTests {
