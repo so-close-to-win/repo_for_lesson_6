f4b25c0ca6dab6b25cd0cd9a3a16094babae2db1
Andrey Kulikov <9230697@gmail.com>
2020-09-04 14:59:53 +0300

    1

--- src/test/java/tests/GoogleTest.java
+++ src/test/java/tests/GoogleTest.java
@@ -20,47 +20,49 @@ class GoogleTest {
         $("html").shouldHave(text("ru.selenide.org"));
     }
 
-    @Test
-    void selenideSearchTestX2() {
-        // Открыть google
-        open("https://google.com");
-
-        // Ввести Покемоны в поиск
-        $(byName("q")).setValue("Pokemon").pressEnter();
-
-        // Проверить, что Покемоны появился в результатах поиска
-        $("html").shouldHave(text("www.pokemon.com"));
-
-        // Очистить поле поиска
-        $(byName("q")).clear();
-
-        // Ввести how work clear() selenide в поиск
-        $(byName("q")).setValue("how work clear() selenide").pressEnter();
-
-        // Проверить, что stackoverflow появился в результатах поиска
-        $("html").shouldHave(text("stackoverflow.com"));
-    }
 }
 
-class YandexTest {
-    @Test
-    void selenideYandexSearch() {
-        // Открыть Яндекс
-        open("https://yandex.ru/");
-
-        // Ввести Покемоны в поиск
-        $(byName("text")).setValue("Pokemon").pressEnter();
-
-        // Проверить, что Покемоны появился в результатах поиска
-        $("html").shouldHave(text("pokemon.com"));
-
-        // Очистить поле поиска
-        $(byName("text")).clear();
-
-        // Ввести how work clear() selenide в поиск
-        $(byName("text")).setValue("how work clear() selenide").pressEnter();
-
-        // Проверить, что stackoverflow появился в результатах поиска
-        $("html").shouldHave(text("stackoverflow.com"));
-    }
-}
+//    @Test
+//    void selenideSearchTestX2() {
+//        // Открыть google
+//        open("https://google.com");
+//
+//        // Ввести Покемоны в поиск
+//        $(byName("q")).setValue("Pokemon").pressEnter();
+//
+//        // Проверить, что Покемоны появился в результатах поиска
+//        $("html").shouldHave(text("www.pokemon.com"));
+//
+//        // Очистить поле поиска
+//        $(byName("q")).clear();
+//
+//        // Ввести how work clear() selenide в поиск
+//        $(byName("q")).setValue("how work clear() selenide").pressEnter();
+//
+//        // Проверить, что stackoverflow появился в результатах поиска
+//        $("html").shouldHave(text("stackoverflow.com"));
+//    }
+//}
+//
+//class YandexTest {
+//    @Test
+//    void selenideYandexSearch() {
+//        // Открыть Яндекс
+//        open("https://yandex.ru/");
+//
+//        // Ввести Покемоны в поиск
+//        $(byName("text")).setValue("Pokemon").pressEnter();
+//
+//        // Проверить, что Покемоны появился в результатах поиска
+//        $("html").shouldHave(text("pokemon.com"));
+//
+//        // Очистить поле поиска
+//        $(byName("text")).clear();
+//
+//        // Ввести how work clear() selenide в поиск
+//        $(byName("text")).setValue("how work clear() selenide").pressEnter();
+//
+//        // Проверить, что stackoverflow появился в результатах поиска
+//        $("html").shouldHave(text("stackoverflow.com"));
+//    }
+//}
---
+++ src/test/java/tests/YandexTests.java
@@ -0,0 +1,21 @@
+package tests;
+
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byName;
+import static com.codeborne.selenide.Selenide.*;
+
+public class YandexTests {
+    @Test
+    void selenideSearchTest() {
+        // Открыть Yandex
+        open("https://yandex.ru");
+
+        // Ввести Selenide в поиск
+        $("#text").val("Selenide").pressEnter();
+
+        // Проверить, что Selenide появился в результатах поиска
+        $("html").shouldHave(text("ru.selenide.org"));
+    }
+}
